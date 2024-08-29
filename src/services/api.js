3f884f97d2dc452e9b83a1f1b49ecbacc2480a3f
caiosousa2003 <caiosousa080403@gmail.com>
2024-08-29 12:04:27 -0300

    fix: AuthStoreAndApi

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,22 @@
 import axios from "axios";
-//import useAuthStore from "../stores/auth";
+import useAuthStore from "../stores/auth";
 
 const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}`;
 const api = axios.create({
   baseURL: BASE_URL,
 });
 
-// api.interceptors.request.use(
-//   (req) => {
-//     const { getToken } = useAuthStore.getState();
-//     const token = getToken();
+api.interceptors.request.use(
+  (config) => {
+    const { auth } = useAuthStore.getState();
+    if (!config.headers.Authorization && auth?.accessToken) {
+      config.headers.Authorization = `Bearer ${auth.accessToken}`;
+    }
+    
+    return config;
+  },
 
-//     if (!req.headers.Authorization && token) {
-//       req.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return req;
-//   },
-
-//   (error) => Promise.reject(error)  
-// );
+  (error) => Promise.reject(error)  
+);
 
 export default api;
--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,26 +1,31 @@
 //Auth.js
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 import { jwtDecode } from "jwt-decode";
 
-const useAuthStore = create(
-  persist(
-    (set) => ({
-      token: null,
-      usuario: null,
-      setToken: (token) => {
-        const { usuario } = jwtDecode(token);
-        set({ token, usuario });
-      },
-      setUsuario: (usuario) => set({ usuario }),
-      logout: () => {
-        set({ token: null, usuario: null });
-      },
-    }),
-    {
-      name: "auth",
-    }
-  )
-);
+const useAuthStore = create((set) => ({
+    auth: null,
+    setAuth: (acessToken) => {
+      const { user,
+              iat: issuedAtTimestamp,
+              exp: expireAtTimestamp, } = jwtDecode(acessToken);
+      
+      const secureTime = 5 * 60;
+      const expireIn = expireAtTimestamp - issuedAtTimestamp - secureTime;
+      
+      set((state) => ({
+        auth: {
+          ...state.auth,
+          acessToken,
+          user,
+          expireIn,
+        },
+      }));
+    },
+    
+    setUser: (user) => set((state) => ({ auth: { ...state?.auth, user }})),
+
+    clearAuth: () => set({ auth: null }),
+
+}));
 
 export default useAuthStore;
