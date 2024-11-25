04e8eb916b86d561be7465a0d65cd9ab7e12da2d
caiosousa2003 <caiosousa080403@gmail.com>
2024-11-25 01:40:54 -0300

    fix: AddStephanieStyles

--- src/components/common/FormTimePicker/Styles.js
+++ src/components/common/FormTimePicker/Styles.js
@@ -1,50 +1,36 @@
 import { TimePicker } from "antd";
 import styled from "styled-components";
+import { colors, fonts } from "../../../styles/stylesVariables";
 
 export const Container = styled.div``;
 
 export const StyledTimePicker = styled(TimePicker)`
   width: 100%;
-  border-radius: 8px;
-  padding: 8px;
-  border: 1px solid #ccc;
+  border-radius: 10px;
+  padding: 0.7rem 3rem;
+  background-color: ${colors.background.secondary};
+  color: ${colors.white};
+  border: 0.15rem solid transparent;
+  font-family: ${fonts.Exo2};
+  cursor: pointer;
   transition: all 0.3s ease;
-  background-color: #FFFFFF;
-  transition: 1s ease transform;
 
-  &:hover, &:focus, &:active {
-    border-color: #007bff;
-    box-shadow: 0 0 0 2px rgba(0, 123, 255, 0.25);
-    background-color: #FFFFFF;
-    transform: scale(1.1);
+  &:hover {
+    background-color: ${colors.black};
+    color: ${colors.white};
   }
 
-  .ant-picker-content{
-    background-color: red;
-  }
-  
   .ant-picker-input > input {
     font-size: 16px;
-    color: #333;
+    color: ${colors.white} !important;
   }
 
   .ant-picker-suffix {
-    color: #007bff; 
-  }
-  .ant-picker-time-panel-cell{
-    color: green;  
-    background-color: red;
+    color: ${colors.white} !important;
   }
 
-  .ant-picker-panel-container {
-    .ant-picker-time-panel-column > li.ant-picker-time-panel-cell-selected {
-      background-color: red; 
-      color: white;
-    }
-
-    .ant-picker-time-panel-column > li.ant-picker-time-panel-cell:hover {
-      background-color: red; 
-      color: green;
-    }
+  .ant-picker-input > input::placeholder {
+    color: #f6fff8 !important;
+    opacity: 1;
   }
 `;
--- src/components/common/UploadButton/Styles.js
+++ src/components/common/UploadButton/Styles.js
@@ -1,12 +1,28 @@
 import styled from "styled-components";
 import { Button } from "antd";
+import { colors, fonts } from "../../../styles/stylesVariables";
 
 export const AddButton = styled(Button)`
-  width: auto;
-  height: 40px;
+  display: flex;
   align-items: center;
   justify-content: center;
-  color: #6b9080;
-  background-color: white;
+  width: 200px;
+  height: 50px;
+  padding: 0.7rem 3rem;
   border-radius: 10px;
+  background-color: ${colors.background.secondary};
+  color: ${colors.font.primary};
+  border: 0.15rem solid transparent;
+  font-family: ${fonts.Exo2};
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:hover {
+    background-color: ${colors.black};
+    color: #f6fff8 !important;
+  }
+
+  &:focus {
+    outline: none;
+  }
 `;
